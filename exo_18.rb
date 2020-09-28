571e4537f7a422133ad5e8dce6fb4524fcc436b8
Martin Moradi <moradi.martin@gmail.com>
2020-09-28 21:41:18 +0200

    Exo 1 à 18 ok

---
+++ exo_18.rb
@@ -0,0 +1,18 @@
+#Écris un programme exo_18.rb qui va créer une liste de 50 faux emails et les stocker dans une array. Voici le format que devront avoir les faux emails :
+
+
+arr_emails = []
+
+i = 00
+50.times do   
+    if i < 10 
+        email = "jean.dupont.0"+i.to_s+"@email.fr" 
+    else
+    email = "jean.dupont."+i.to_s+"@email.fr" 
+    end
+    arr_emails << email
+    i+=1
+end
+
+prints arr_emails
+return arr_emails
