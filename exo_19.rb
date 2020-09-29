5edcbb5a98d242333da3638d6257fae0f006d284
Martin Moradi <moradi.martin@gmail.com>
2020-09-29 10:56:41 +0200

    Terminé tous les exos git add .

--- exo_19.rb
+++ exo_19.rb
@@ -1,7 +1,6 @@
 #Prends le programme exo_18.rb et créé un programme exo_19.rb qui va reprendre l'array des emails créés, et n'afficher que les emails avec un nombre pair.
-
 arr_emails = []
-i = 00
+i = 0
 50.times do   
     if i < 10 
         email = "jean.dupont.0"+i.to_s+"@email.fr" 
@@ -12,5 +11,19 @@ i = 00
     i+=1
 end
 
-arr_emails.each do |ele| 
-    
+new_arr = []
+arr_emails.each do |mail|
+    parts = mail.split("@")
+    firstPart = parts[0]
+    secondPart = parts[1]
+    number = firstPart[-1].to_i
+    if number.even?
+     new_arr << mail
+    end
+end
+print new_arr
+
+# on veut 1 -> iterate dans l'array pour chaque element (mail)
+# split l'element au niveau du @ 
+# pour vérifier si le last index du premier cut % 2 == 0
+# si oui, retourner dans un nouvel array
